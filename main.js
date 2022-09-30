3f9ba9d2fd6a0b6a1fb57a7aaf0e863f2555923c
Michelle Sandner <michelle.sandner@springernature.com>
2022-09-30 15:45:55 +0200

    added popup + new round fix

--- main.js
+++ main.js
@@ -1,106 +1,137 @@
-let inputWord = "QUALLENBISS";
+let inputWord = "ABC";
 let clickCount = 1;
 let allClick = false;
 let dead = false;
 
+document.querySelector("#submitButton").addEventListener("click", clicksubmitButton);
+document.querySelector("#closingButton").addEventListener("click", closeButton);
 
-createDivs(inputWord);
+
+console.log(inputWord)
+
 
 document.querySelectorAll(".ABC").forEach(item => {
     item.addEventListener("click", handleClickABC);
 })
 
-
-
 function handleClickABC(e){
 
+ let prove = false;
+ 
+  let shortCut = e.target;
+  let currentClickedItem;
 
-  document.querySelectorAll(".item_input").forEach(item => {
-    if(!item.classList.contains("guessed")){
+  currentClickedItem = shortCut.parentNode.id;
 
-      allClick = false;
-      return;
+  
+
+  if(!document.getElementById(currentClickedItem).classList.contains("disable")){
+    
+    if(inputWord.includes(currentClickedItem)){
+      for(i = 0; i < inputWord.length; i++){
+        
+        if( inputWord[i] == currentClickedItem){
+          document.querySelector("#item" + i).innerText = inputWord[i];
+          document.querySelector("#item" + i).classList.add("visible");
+          document.querySelector("#item" + i).classList.add("guessed");
+          document.getElementById(currentClickedItem).classList.add("disable");
+          
+        }
+      }   
     }else{
-      allClick = true;
+
+      document.querySelector("#hang_manArea > img").src = './img/' + clickCount + 'dead.png';
+      if(clickCount == 10){
+        alert("its dead!!!!")
+        setTimeout( () => {
+          restartGame();
+        }, 1000 );
+        return; 
+      }
+      clickCount = clickCount + 1;
     }
+  }
+  document.getElementById(currentClickedItem).classList.add("disable");
+  prove = proveIfAllGuessed();
+  if(prove){
+    alert("you won")
+    setTimeout( () => {
+      restartGame();
+    }, 1000 );
+  }
+}
+
+
+function restartGame(){
+
+  clickCount = 0;
+  document.querySelector("#hang_manArea > img").src = './img/' + 0 + 'dead.png';
+  document.querySelectorAll(".ABC").forEach(item => {
+    if(item.classList.contains("disable")){
+      item.classList.remove("disable");
+    }
+
+    document.querySelector("#PopUpName").style.display = "flex";
+
+    document.querySelectorAll(".lines").forEach(item => {
+      item.innerHTML = "";
+    })
   })
 
-    let shortCut = e.target;
-    let currentClickedItem;
+  
+  
+}
 
-    currentClickedItem = shortCut.parentNode.id;
+function createDivs(inputWord){
 
-    
-
-    if(!document.getElementById(currentClickedItem).classList.contains("disable")){
-      
-        if(inputWord.includes(currentClickedItem)){
-          for(i = 0; i < inputWord.length; i++){
-           
-            if( inputWord[i] == currentClickedItem){
-                document.querySelector("#item" + i).innerText = inputWord[i];
-                document.querySelector("#item" + i).classList.add("visible");
-                document.querySelector("#item" + i).classList.add("guessed");
-                document.getElementById(currentClickedItem).classList.add("disable");
-            }
-          }   
-        }else{
-
-          document.querySelector("#hang_manArea > img").src = './img/' + clickCount + 'dead.png';
-          if(clickCount == 10){
-            alert("its dead!!!!")
-            setTimeout( () => {
-              restartGame();
-          }, 1000 );
-            return;
-            
-          }
-          clickCount = clickCount + 1;
-        }
-    }
-      document.getElementById(currentClickedItem).classList.add("disable");
+  if(inputWord.length >= 7){
+    document.querySelector("#line2").classList.remove("hidden");
+  }else{
+    document.querySelector("#line2").classList.add("hidden");
   }
 
-
-  function restartGame(){
-
-    clickCount = 0;
-    document.querySelector("#hang_manArea > img").src = './img/' + 0 + 'dead.png';
-    document.querySelectorAll(".ABC").forEach(item => {
-      if(item.classList.contains("disable")){
-        item.classList.remove("disable");
-      }
-
-      document.querySelectorAll(".item_input").forEach(item => {
-        item.innerText = "_";
-        item.classList.remove("visible");
-        item.classList.remove("guessed");
-      })
-
-    })
-  }
-
-  function createDivs(inputWord){
-
-    if(inputWord.length >= 7){
-      document.querySelector("#line2").classList.remove("hidden");
+  for(k = 0; k < inputWord.length; k++){
+    if(k >= 7 && k <= 14){
+      div = '<div id="item'+ k +'" class="item_input"><span>-</span></div>';
+      document.querySelector("#line2").innerHTML += div;
     }else{
-      document.querySelector("#line2").classList.add("hidden");
-    }
-
-    for(k = 0; k < inputWord.length; k++){
-      if(k >= 7 && k <= 14){
+      if(k == 0){
         div = '<div id="item'+ k +'" class="item_input"><span>-</span></div>';
-        document.querySelector("#line2").innerHTML += div;
+        document.querySelector("#line1").innerHTML += div;
       }else{
-        if(k == 0){
-          div = '<div id="item'+ k +'" class="item_input"><span>-</span></div>';
-          document.querySelector("#line1").innerHTML += div;
-        }else{
-          div = '<div id="item'+ k +'" class="item_input"><span>-</span></div>';
-          document.querySelector("#line1").innerHTML += div;
-        }
+        div = '<div id="item'+ k +'" class="item_input"><span>-</span></div>';
+        document.querySelector("#line1").innerHTML += div;
       }
-      
-    }
+    } 
   }
+}
+
+
+function proveIfAllGuessed(){
+  let tested = true;
+  document.querySelectorAll(".item_input").forEach(item => {
+    if(!(item.classList.contains("guessed"))){
+      tested = false;
+    }
+  })
+  return tested;
+}
+
+function GetValueInput(){
+
+  console.log("h");
+
+  let input = document.querySelector("#inputName").value;
+
+  console.log(input)
+  document.querySelector("#PopUpName").style.display = "none";
+  return input;
+}
+
+function clicksubmitButton(){
+  let input = GetValueInput();
+
+  inputWord = input;
+
+  createDivs(inputWord);
+}
